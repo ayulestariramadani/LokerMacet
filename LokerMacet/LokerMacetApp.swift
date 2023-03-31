f793d1b0434f2a9eb17ed18bc410aacfebc15dd2
ayulestariramadani <ayulestariramadani@gmail.com>
2023-03-31 14:17:49 +0700

    Initial Commit

---
+++ LokerMacet/LokerMacetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LokerMacetApp.swift
+//  LokerMacet
+//
+//  Created by Ayu Lestari Ramadani on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LokerMacetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
